4f42523779eb1c43e2f287e0b19184ff4a588f6f
Erik5231 <90755554+Erik5231@users.noreply.github.com>
2021-09-20 00:18:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let money = 0;
+
 let upgradePrice = 50;
 let upgrades = 0;
+let ammount = 1;
+
 let upgradePriceAutoclicker = 125;
 let upgradesAutoclicker = 0;
-let ammount = 1;
 let ammountAutoclicker = 0;
+
+let inadaptablePrice = 0;
+
+let upgradePricegender = 1500;
+let upgradesGender = 0;
+let ammountAutoclickergender = 0;
 
 function cheat() {
     if (money < 99999) {
@@ -36,7 +44,7 @@ function buyAutoclicker1() {
     if (money >= upgradePriceAutoclicker) {
         ammountAutoclicker = ammountAutoclicker + 1;
         money = money - upgradePriceAutoclicker;
-        upgradePriceAutoclicker = upgradePriceAutoclicker + 150;
+        upgradePriceAutoclicker = upgradePriceAutoclicker + 160;
         upgradesAutoclicker++;
         document.getElementById("upgradePriceAutoclicker").innerHTML = upgradePriceAutoclicker;
         document.getElementById("upgradesAutoclicker").innerHTML = upgradesAutoclicker;
@@ -58,4 +66,34 @@ function buyUpgrade1() {
 
 function updateMoney() {
     document.getElementById("money").innerHTML = money;
+}
+
+function buyInadaptable() {
+    if(money >= inadaptablePrice) {
+        money = money - 100;
+        document.getElementById("inadaptablePrice").innerHTML = inadaptablePrice;
+        updateMoney();
+    }
+}
+
+function buyGender() {
+    if(money >= upgradePricegender) {
+        ammountAutoclickergender = ammountAutoclickergender + 1;
+        money = money - upgradePricegender;
+        upgradePricegender = upgradePricegender + 1750;
+        upgradesGender++;
+        document.getElementById("upgradePricegender").innerHTML = upgradePricegender;
+        document.getElementById("upgradesGender").innerHTML = upgradesGender;
+    }
+    updateMoney();
+}
+
+function intervalMoney2 () {
+    money = money + ammountAutoclickergender;
+    if (ammountAutoclickergender > 0) {
+        document.getElementById("lgbtClick").style.color = "rgba(255,255,255,1)";
+        setTimeout(clearFont, 200);
+    }
+    setTimeout(intervalMoney2, 1000);
+    updateMoney();
 }
