702cd676ddaff3c2543e0afb4f2b43574ee53ea5
Lil JSF <90755554+Erik5231@users.noreply.github.com>
2021-10-03 19:08:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-let money = 0;
+ let money = 0;
 
-let upgradePrice = 50;
-let upgrades = 0;
-let ammount = 1;
-let ammountAutoqueer = 0;
+ let upgradePrice = 50;
+ let upgrades = 0;
+ let ammount = 1;
+ let ammountAutoqueer = 0;
 
-let upgradePriceAutoclicker = 350;
-let upgradesAutoclicker = 0;
-let ammountAutoclicker = 0;
+ let upgradePriceAutoclicker = 350;
+ let upgradesAutoclicker = 0;
+ let ammountAutoclicker = 0;
 
-let upgradePricegender = 2250;
-let upgradesGender = 0;
-let ammountAutoclickergender = 0;
+ let upgradePricegender = 2250;
+ let upgradesGender = 0;
+ let ammountAutoclickergender = 0;
+
+ let multiplier = 1;
+
+ let qps = 0 * multiplier;
+ let qpc = 1 * multiplier;
+ let multi = 1;
+
+ let upgradePricemulti = 1000000;
+ let upgradesMultiplier = 0;
+
+ let turboPrice = 10;
+
+ let clicks = 0;
 
 
-let qps = 0;
-let qpc = 1;
+ let menuSwitch = 0;
 
-let menuSwitch = 0;
 
 
 function changeText() { 
-   let change = document.getElementById("cheatButton");
+    change = document.getElementById("cheatButton");
    change.innerHTML = "We appreciate your support!";
 }
 
 function cheat() {
-        money = + 99999;
+        money = + 9999999;
         updateMoney();
         var audio = new Audio('dobryDen.mp3');
         audio.play();
@@ -34,7 +45,9 @@ function cheat() {
 
 function addMoney(ammount) {
     money = money + ammount;
+    clicks++;
     updateMoney();
+    QPS();
 }
 
 function intervalMoney() {
@@ -48,14 +61,12 @@ function intervalMoney() {
     save();
 }
 
-/*
-function intervalMoney2 () {
-    money = money + ammountAutoclickergender;
-    if (ammountAutoclickergender > 0) {
-        document.getElementById("lgbtClick").style.color = "rgba(255,255,255,1)";
-        setTimeout(clearFont, 200);
+
+/*function bezdekMode () {
+    money = money + bezdekBooster;
+    if (bezdekBooster > 0) {
     }
-    setTimeout(intervalMoney2, 1000);
+    setTimeout(bezdekMode, 10);
     updateMoney();
 }
 */
@@ -66,7 +77,7 @@ function clearFont() {
 
 function buyAutoclicker1() {
     if (money >= upgradePriceAutoclicker) {
-        ammountAutoclicker = ammountAutoclicker + 5;
+        ammountAutoclicker = ammountAutoclicker + 10 * multiplier;
         money = money - upgradePriceAutoclicker;
         upgradePriceAutoclicker = upgradePriceAutoclicker + 380;
         upgradesAutoclicker++;
@@ -82,8 +93,8 @@ function buyAutoclicker1() {
 
 function buyUpgrade1() {
     if (money >= upgradePrice) {
-        ammount = ammount + 1;
-        ammountAutoqueer = ammountAutoqueer + 1;
+        ammount = ammount + 1*multiplier;
+        ammountAutoqueer = ammountAutoqueer + 1*multiplier;
         money = money - upgradePrice;
         upgradePrice = upgradePrice + 205;
         upgrades++;
@@ -93,6 +104,7 @@ function buyUpgrade1() {
         qps = qps + 1;
         updateMoney();
         QPS();
+        multi();
     } else {
         showRadek();
     }
@@ -113,7 +125,7 @@ function buyInadaptable() {
 
 function buyGender() {
     if(money >= upgradePricegender) {
-        ammountAutoclickergender = ammountAutoclickergender + 20;
+        ammountAutoclickergender = ammountAutoclickergender + 25*multiplier;
         money = money - upgradePricegender;
         upgradePricegender = upgradePricegender + 2550;
         upgradesGender++;
@@ -142,10 +154,14 @@ function menuShow() {
 }
 
 function QPS() {
-    document.getElementById("qps").innerHTML = qps;
+    document.getElementById("qps").innerHTML = qps*multiplier;
     document.getElementById("qpc").innerHTML = qpc;
-    document.getElementById("qps2").innerHTML = qps;
+    document.getElementById("qps2").innerHTML = qps*multiplier;
     document.getElementById("qpc2").innerHTML = qpc;
+    document.getElementById("multi").innerHTML = multiplier;
+    document.getElementById("multi2").innerHTML = multiplier;
+    document.getElementById("clicks").innerHTML = clicks;
+    document.getElementById("clicks2").innerHTML = clicks;
 }
 
 function showRadek() {
@@ -158,6 +174,48 @@ function showRadek() {
 function hideRadek() {
     document.getElementById("raderror").style.display = "none";
     document.getElementById("raderror").style.opacity = "0";
+}
+
+function multiply() {
+    if(money >= upgradePricemulti)
+    {
+     money = 0;
+     multiplier = multiplier + 0.5;
+     upgradesMultiplier++;
+
+     upgradePrice = 50;
+     upgrades = 0;
+     ammount = 1 * multiplier;
+     ammountAutoqueer = 0;
+
+     upgradePriceAutoclicker = 350;
+     upgradesAutoclicker = 0;
+     ammountAutoclicker = 0;
+
+     upgradePricegender = 2250;
+     upgradesGender = 0;
+     ammountAutoclickergender = 0;
+
+     qps = 0 * multiplier;
+     qpc = 1 * multiplier;
+     
+
+    
+
+     upgradePricemulti = 1000000;
+     document.getElementById("upgradesMultiplier").innerHTML = upgradesMultiplier;
+     document.getElementById("upgradePricemulti").innerHTML = upgradePricemulti;
+     document.getElementById("upgradePricegender").innerHTML = upgradePricegender;
+     document.getElementById("upgradesGender").innerHTML = upgradesGender;
+     document.getElementById("upgradePrice").innerHTML = upgradePrice;
+     document.getElementById("upgrades").innerHTML = upgrades;
+     document.getElementById("upgradePriceAutoclicker").innerHTML = upgradePriceAutoclicker;
+     document.getElementById("upgradesAutoclicker").innerHTML = upgradesAutoclicker;
+    } else {
+        showRadek();
+    }
+    updateMoney();
+    QPS();
 }
 
 /*function playAudio() {
@@ -212,4 +270,14 @@ function load() {
     qpc = parseInt(qpc);
     updateMoney();
     QPS();
+}
+
+function turboStart() {
+    turbo = setInterval(function() {
+            addMoney(ammount);
+    }, 4);
+}
+
+function turboEnd() {
+    clearInterval(turbo);;
 }
